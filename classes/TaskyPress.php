16cdfe194513aba1106ce15fcca11ad4116f8acf
hoplitfr <contact@hoplit.fr>
2025-02-06 22:11:38 +0100

    Creating a new table when activating the plugin
    When activating the plugin a new SQL table for the tasks is created.

--- classes/TaskyPress.php
+++ classes/TaskyPress.php
@@ -12,7 +12,7 @@ class TaskyPress
     }
 
     /**
-     * Create custom roles when activating the plugin.
+     * Create custom roles and table when activating the plugin.
      *
      * @return void
      */
@@ -20,6 +20,7 @@ class TaskyPress
     {
         add_role('task_performer', 'Task Performer', array('read' => true));
         add_role('task_provider', 'Task Provider', array('read' => true));
+        $this->create_tasks_table();
     }
 
     /**
@@ -31,6 +32,40 @@ class TaskyPress
     public function enqueue_styles(): void
     {
         wp_enqueue_style('taskypress-main-style', TASKYPRESS_PLUGIN_URL . 'css/main.css');
+    }
+
+    /**
+     * Create SQL table for the tasks.
+     *
+     * @return void
+     */
+    private function create_tasks_table(): void
+    {
+        global $wpdb;
+
+        $table_name = $wpdb->prefix . 'taskypress_tasks';
+
+        $charset_collate = $wpdb->get_charset_collate();
+
+        $sql = "CREATE TABLE $table_name (
+        id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+        task_provider_id BIGINT(20) UNSIGNED NOT NULL,
+        task_performer_id BIGINT(20) UNSIGNED NOT NULL,
+        task_title VARCHAR(255) NOT NULL,
+        task_description TEXT NOT NULL,
+        task_status VARCHAR(50) NOT NULL DEFAULT 'pending',
+        task_progress TINYINT(3) UNSIGNED NOT NULL DEFAULT 0,
+        task_additional_info_requests TEXT DEFAULT NULL,
+        task_created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        task_updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        PRIMARY KEY (id)
+    ) $charset_collate;";
+
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
+        if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
+            dbDelta($sql);
+        }
     }
 
 }
